cf3a6175df9eb50201a77f52073e5f7f849d7040
NickScanlan <nicholas_scanlan@yahoo.com>
2022-08-29 14:59:01 -0700

    loop basic 1 finished

---
+++ Bootcamp/Python/fundamentals/fundamentals/for_loop_basic1.py
@@ -0,0 +1,32 @@
+# for a in range(151):
+#     print(a)
+
+
+# for b in range(5, 1005, 5):
+#     print(b)
+
+
+# for c in range(101):
+#     if c % 10 == 0:
+#         print('Coding Dojo')
+#     elif c % 5 == 0:
+#         print('Coding')
+#     else:
+#          print(c)
+
+# for d in range(500000):
+#     if d % 2 == 1:
+#         d += d
+# print(d)        
+
+# for e in range(2018, -1, -4):
+#     print(e) 
+        
+
+lowNum = 2
+highNum = 9
+mult = 3
+
+for e in range(lowNum, highNum + 1):
+    if e % mult == 0:
+        print(e)
