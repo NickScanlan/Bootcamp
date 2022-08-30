1ee780b3865812d192d94a2d46056537a2fcce39
NickScanlan <nicholas_scanlan@yahoo.com>
2022-08-29 21:45:17 -0700

    finsihed core nested dictionaries

---
+++ Bootcamp/Python/fundamentals/fundamentals/nested_dictionaries_and_lists.py
@@ -0,0 +1,61 @@
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Andres', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 30} ]
+
+x[1][0] = 15
+print(x)
+
+
+students[0]['last_name'] = 'Bryant'
+print(students[0])
+
+sports_directory['soccer'][0] = 'Andres'
+print(sports_directory['soccer'])
+
+z[0]['y'] = 30
+print(z)
+
+
+
+students = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+
+def iterateDictionary(list):
+    for ft in list:
+        for key in ft:
+            print(f"{key}: {ft[key]}")
+
+iterateDictionary(students)
+
+
+dojo = {
+   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+def printInfo(dict):
+    print(len(dict['locations']), "LOCATIONS")
+    for f in dict['locations']:
+        print(f)    
+    print(len(dict['instructors']), "INSTRUCTORS")
+    for g in dict['instructors']:
+        print(g)
+printInfo(dojo)
+
+
+
+
+
+
+
