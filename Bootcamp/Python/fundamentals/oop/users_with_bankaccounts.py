f406fab9d85dd2183c25e914c9816a20af7426d5
NickScanlan <nicholas_scanlan@yahoo.com>
2022-08-31 21:34:28 -0700

    users with back accounts

--- Bootcamp/Python/fundamentals/oop/users_with_bankaccounts.py
+++ Bootcamp/Python/fundamentals/oop/users_with_bankaccounts.py
@@ -1,12 +1,32 @@
-class User:		
-    def __init__(self):
-        self.first_name = "Ada"
-        self.last_name = "Lovelace"
-        self.age = 42
+class BankAccount:
+    def __init__(self, checking, savings):
+        self.checking = checking(int_rate = 0.2 * checking)
+        self.savings = savings
+        
 
-user_ada = User()
-print(user_ada.last_name)
+ 
 
-# Create another user called user_2!
-user_2 = User()
-print(user_2.last_name) 
+
+
+class User:
+    def __init__(self,first_name, last_name, BankAccount):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.checking = BankAccount.checking
+        self.savings = BankAccount.savings
+    def display_info(self):
+        print(f"{self.first_name}, {self.last_name}")
+        return self
+
+
+
+        
+nick = User("nick", "scan", "jfkds@fad.com", 342)
+user2 = User("user", "two", "jfkds@fad.com", 432)
+user3 = User("user", "three", "jfkds@fad.com", 332)
+
+nick.display_info()
+print('-------------------------------------------------------------')
+user2.display_info()
+print('-------------------------------------------------------------')
+user3.display_info()
