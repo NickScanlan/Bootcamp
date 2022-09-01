d7aeabd271b81867c2a95b68485f8976f78e99fc
NickScanlan <nicholas_scanlan@yahoo.com>
2022-08-31 23:35:31 -0700

    fds

--- Bootcamp/Python/fundamentals/oop/users_with_bankaccounts.py
+++ Bootcamp/Python/fundamentals/oop/users_with_bankaccounts.py
@@ -1,37 +1,3 @@
-class BankAccount:
-    def __init__(self, checkingAmount, savingsAmount):
-        rate = 0.2
-        self.checking = checkingAmount(int(rate) * int(checkingAmount))
-        self.savings = savingsAmount
-
-
- 
-
-
-
-class User:
-    def __init__(self,first_name, last_name, BankAccount):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.checking = BankAccount.checking
-        self.savings = BankAccount.savings
-    def display_info(self):
-        print(f"{self.first_name}, {self.last_name}")
-        return self
-    def make__deposit(self):
-        
-
-
-        
-nick = User("nick", "scan", "jfkds@fad.com", 342, BankAccount (100,100))
-# user2 = User("user", "two", "jfkds@fad.com", 432)
-# user3 = User("user", "three", "jfkds@fad.com", 332)
-
-nick.display_info()
-print('-------------------------------------------------------------')
-# user2.display_info()
-# print('-------------------------------------------------------------')
-# user3.display_info()
 
 
 # Add a make_deposit method to the User class that calls on it's bank account's instance methods.
