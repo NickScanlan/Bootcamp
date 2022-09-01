a8bd30a0707b1722a050df0228caa0cc77c7abfc
NickScanlan <nicholas_scanlan@yahoo.com>
2022-09-01 10:57:13 -0700

    update

--- Bootcamp/Python/flask/flask_notes/notes.py
+++ Bootcamp/Python/flask/flask_notes/notes.py
@@ -21,3 +21,18 @@
     step 6: then we can run exit to leave the shell
     when were done working in the environment we run exit to leave
 """
+
+
+"""
+virtual environments: used to control versions of our packages for our apps, walled off from the rest of our computer
+pip install pipenv
+pip3 install pipenv
+install pipenv -- run this once ever to globally install pipenv
+python -m pipenv <command to use>
+python3 -m pipenv <command to use>
+may have to run pipenv commands like above depending on your computer set up
+pipenv install flask
+this command creates a new virtual environment and installs flask to it
+exit
+leaves virtual environmnt
+"""
