5250c97c81394fc1ce561d07837ffd88e2102c01
NickScanlan <nicholas_scanlan@yahoo.com>
2022-09-04 13:22:07 -0700

    finished counter

--- Bootcamp/Python/flask/countdown/server.py
+++ Bootcamp/Python/flask/countdown/server.py
@@ -1,9 +1,28 @@
-from flask import Flask, render_template, redirect, session
+from flask import Flask, render_template, session, redirect
 app = Flask(__name__)   
 app.secret_key = "bababooey"
 
+@app.route('/')
+def home():
+    if 'bababooey' in session:  
+        session['count'] = 0
+    session['count'] += 0
+    return render_template('index.html', count = session['count'])
 
 
+
+
+@app.route('/add')
+def add():
+    print('this is the counter page')
+    session['count'] += 1
+    return redirect('/')
+
+
+@app.route('/destroy')
+def reset():
+    session['count'] = 1
+    return redirect('/') 
 
 
 
