7d7309934e3c91feadbd78ce0cb78fabdaa87cf4
NickScanlan <nicholas_scanlan@yahoo.com>
2022-09-25 21:33:10 -0700

    fund1

---
+++ Bootcamp/CSharp/fund1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+// for(int i = 1; i <= 255; i++){
+//     Console.WriteLine(i);
+// }
+
+for(int i = 1; i <= 100; i++){
+    if (i % 5 && 3 == 0){
+        Console.WriteLine("fizzbuzz");
+    }
+    else if (i % 5  == 0){
+        Console.WriteLine("fizz");
+    }
+    else if (i % 3 == 0){
+        Console.WriteLine("buzz");
+    }
+}
