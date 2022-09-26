de98f7262223c00bcb3d44b03736f74e5b02e053
NickScanlan <nicholas_scanlan@yahoo.com>
2022-09-26 10:37:28 -0700

    language fundamentals

--- Bootcamp/CSharp/LangFund/Program.cs
+++ Bootcamp/CSharp/LangFund/Program.cs
@@ -1,38 +1,67 @@
-﻿//compiled languages are very good at telling you whats wrong with your code. 
+﻿//general naming conventions
+    //variables, parameters, and fields are camelCase
+    //everything else is PascalCase
 
-// create a new loop that prints to 100
-// void means not returning anything from the function
-//int means returning integer
-//string returns string
-//etc
-
-// void PrintNumbers()
-// {
-//     Console.WriteLine("test");
-// }
-
-
- 
-
-
-//for loops
-
-void PrintNumbers()
+// create a new loop that prints 1-100
+void PrintNumbers(int start = 1, int inclusiveEnd = 100) 
 {
-    for (int i = 1; i <= 100; i++)
+    for (int i = start; i <= inclusiveEnd; i++)
     {
-        bool isDivisableBy3 = i % 3 == 0;
-        bool isDivisableBy5 = i % 5 == 0;
+        bool isDivisibleBy3 = i % 3 == 0;
+        bool isDivisibleBy5 = i % 5 == 0;
+        bool isDivisibleBy3And5 = isDivisibleBy3 && isDivisibleBy5;
+        bool isDivisibleBy3Or5 = isDivisibleBy5 || isDivisibleBy3;
+        
+        //if divisible by 3 & 5, nothing
+        //if divisible by 3
+        //if divisible by 5
 
-        //interpalated strings
-        Console.WriteLine($"is {i} divisible by 3? {isDivisableBy3}");
-        Console.WriteLine($"is {i} divisible by 5? {isDivisableBy5}");
+        // ^ is the exclusive or operator, meaning ONLY one can be true
+        // if(isDivisibleBy3 ^ isDivisibleBy5)
+        // {
+        //     Console.WriteLine(i);
+        // }
+
+        //if BOTH divisibleBy3 & divisibleBy5 aren't true, check if one is true & log it
+        if (isDivisibleBy3Or5 && !isDivisibleBy3And5)
+        {
+            Console.WriteLine(i);
+        }
+
+        // Console.WriteLine($"is {i} divisible by 3? {isDivisibleBy3}");
+        // Console.WriteLine($"is {i} divisible by 5? {isDivisibleBy5}");
     }
 }
-PrintNumbers();
+
+PrintNumbers(80, 120);
 
 
+/*
+Create a list with the values: Todd, Tiffany, Charlie, Geneva, Sydney
+Return a list that only includes names longer than 5 characters
+*/
+List<string> names1 = new List<string>()
+{
+    "Todd", "Tiffany", "Charlie", "Geneva", "Sydney"
+};
 
-//general naming conventions
-    // variables, parameters, and fields are CamelCase "thisVariable"
-    //everything else is PascalCode "ThisCode"
+List<string> FilterToLongNames(List<string> names, int minLength = 5)
+{
+
+    List<string> longNames = new List<string>();
+
+    foreach (string name in names)
+    {
+        if (name.Length > minLength)
+        {
+            longNames.Add(name);
+        }
+    }
+
+    return longNames;
+}
+
+List<string> namesMoreThan5Chars = FilterToLongNames(names1);
+List<string> namesMoreThan6Chars = FilterToLongNames(names1, 6);
+
+Console.WriteLine(String.Join(", ", namesMoreThan6Chars));
