84e07be854abc77fda54cc10db3579186046fb05
NickScanlan <nicholas_scanlan@yahoo.com>
2022-10-26 16:31:51 -0700

    finished first react

---
+++ Bootcamp/MERN/react/First_JSX/first-react/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>
+        Hello Dojo!
+      </h1>
+      <h2>things I need to do:</h2>
+      <ul>
+        <li>
+          Learn React   
+        </li>
+        <li>
+          Climb Mt. Everest
+        </li>
+        <li>
+          Run a Marathon
+        </li>
+        <li>
+          Feed the dogs
+        </li>
+        <li>
+          Get a software dev job
+        </li>
+        
+      </ul>
+    </>
+  );
+}
+
+export default App;
