a941ee66b56282a84f0fc3bd2d6925796cf918a6
NickScanlan <nicholas_scanlan@yahoo.com>
2022-11-14 02:42:44 -0800

    .........product manager 3

--- Bootcamp/MERN/FullStack/ProductManager/client/src/App.js
+++ Bootcamp/MERN/FullStack/ProductManager/client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react';
 import Main from './views/Main';
 import Detail from './views/Details';
+import Update from './views/Update'
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route element={<Main/>} path="/products/" />
         <Route element={<Detail/>} path="/products/:id" />
+        <Route element={<Update/>} path="/products/:id/edit"/>
       </Routes>                   
     </div>
   )
--- Bootcamp/MERN/FullStack/ProductManager/client/src/components/ProductList.js
+++ Bootcamp/MERN/FullStack/ProductManager/client/src/components/ProductList.js
@@ -1,12 +1,31 @@
 import React from 'react'
 import axios from 'axios';
 import {Link} from 'react-router-dom'
+
+
+
+
 const ProductList = (props) => {
+    const { removeFromDom } = props;
+    
+    const deleteProduct = (productId) => {
+        axios.delete('http://localhost:8000/api/products/' + productId)
+            .then(res => {
+                removeFromDom(productId)
+            })
+            .catch(err => console.error(err));
+    }
     return (
         <div>
-            {props.product.map( (product, i) =>
+           {props.product.map( (product, i) =>
                 <Link to = {`/products/${product._id}`}><p key={i}> Title: {product.title} </p></Link>
             )}
+                
+            
+            {props.product.map( (product) =>
+                <button onClick={(e)=>{deleteProduct(product._id)}}>Delete</button>
+            )}
+            
         </div>
     )
 }
